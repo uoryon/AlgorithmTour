b70f8ec8610070c2b5180cdb10a60d20e3af69d1
Sū Hǎi <uoryon@gmail.com>
2015-11-02 08:39:36 +0800

    haha

---
+++ leetcode/delete-node-in-a-linked-list.c
@@ -0,0 +1,13 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+void deleteNode(struct ListNode* node) {
+    struct ListNode *p = node->next;
+    node->val = p->val;
+    node->next = p->next;
+    free(p);
+}
